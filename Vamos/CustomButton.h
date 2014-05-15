9a94e7e7a106d479135a43f88c123d0bb566008b
Ferdinator <ferdinandgrapperhaus@MacBook-Pro-van-Ferdinand-2.local>
2014-05-15 23:53:36 +0200

    update load image + Ballonetje Blazen

---
+++ Vamos/CustomButton.h
@@ -0,0 +1,13 @@
+//
+//  CustomButton.h
+//  Vamos
+//
+//  Created by Ferdinand on 15-05-14.
+//  Copyright (c) 2014 Scott Brugmans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomButton : UIButton
+
+@end
