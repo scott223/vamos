7e9d3e151f29efede9fb02624fea221f51512c83
Scott Brugmans <scott.brugmans@gmail.com>
2014-05-15 18:40:09 +0200

    Added the app

---
+++ Vamos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Vamos
+//
+//  Created by Scott Brugmans on 15-05-14.
+//  Copyright (c) 2014 Scott Brugmans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
